8eac5584dcf799f23b81f35b364234d8c3a8c820
BwizLabs <brandon.r.russell.1@gmail.com>
2019-05-24 08:56:39 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactTable from "react-table";
+import 'react-table/react-table.css';
+import 'react-confirm-alert/src/react-confirm-alert.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { cars: [], open: false, message: ''};
+  }
+
+  componentDidMount() {
+
+    fetch('/findall')
+        .then((response) => response.json())
+        .then((responseData) => {
+          this.setState({
+            cars: responseData,
+          });
+        })
+        .catch(err => console.error(err));
+  }
+
+
+  renderEditable = (cellInfo) => {
+    return (
+        <div
+            style={{ backgroundColor: "#fafafa" }}
+            contentEditable
+            suppressContentEditableWarning
+            onBlur={e => {
+              const data = [...this.state.cars];
+              data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+              this.setState({ cars: data });
+            }}
+            dangerouslySetInnerHTML={{
+              __html: this.state.cars[cellInfo.index][cellInfo.column.id]
+            }}
+        />
+    );
+  }
+
+
+  render() {
+
+    const columns = [{
+      Header: 'id',
+      accessor: 'id',
+      Cell: this.renderEditable
+    }, {
+      Header: 'firstName',
+      accessor: 'firstName',
+      Cell: this.renderEditable
+    }, {
+      Header: 'lastName',
+      accessor: 'lastName',
+      Cell: this.renderEditable
+    }]
+
+
+    return (
+        <div className="App">
+        <h2>Test</h2>
+
+          <ReactTable data={this.state.cars} columns={columns}
+                      filterable={true} pageSize={10}/>
+        </div>
+    );
+  }
 }
 
 export default App;
