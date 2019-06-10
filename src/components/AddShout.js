84c438ff9f7909fdc40fc050ad1f8412b7eae964
BwizLabs <brandon.r.russell.1@gmail.com>
2019-06-10 19:39:08 -0400

    added client compression

--- src/components/AddShout.js
+++ src/components/AddShout.js
@@ -5,12 +5,16 @@ import Button from "@material-ui/core/Button";
 import {IP_URL} from '../constants.js';
 import { ToastContainer, toast } from 'react-toastify'; //MIT
 import 'react-toastify/dist/ReactToastify.css'; //MIT
+import imageCompression from 'browser-image-compression';
+import { Progress } from 'react-sweet-progress';
+import "react-sweet-progress/lib/style.css";
+
 
 class AddShout extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { hits: [], shoutEntry: '',  shoutLat: '', shoutLong: '', shoutIp: '', selectedFile: null, shoutImage: null};
+        this.state = { hits: [], shoutEntry: '',  shoutLat: '', shoutLong: '', shoutIp: '', selectedFile: null, shoutImage: null, progressBarStatus: 0};
 
     }
 
@@ -25,38 +29,82 @@ class AddShout extends React.Component {
     handleSubmit = (event) => {
         event.preventDefault();
 
-if(this.state.selectedFile.size <= 2000000 &&
-    (this.state.selectedFile.type === "image/gif" ||
-        this.state.selectedFile.type === "image/png" ||
-        this.state.selectedFile.type === "image/jpg" ||
-        this.state.selectedFile.type === "image/jpeg" )){
-    //Get IP
 
-    fetch(IP_URL)
-        .then((response) => response.json())
-        .then((responseData) => {
+        //Compress PNG and JPEG files
+        if ( this.state.selectedFile.type === "image/png" ||
+            this.state.selectedFile.type === "image/jpg" ||
+            this.state.selectedFile.type === "image/jpeg"){
 
-            this.setState({
-                shoutIp: responseData['ip'],
-            });
+            var imageFile = this.state.selectedFile;
+           // console.log('originalFile instanceof Blob', imageFile instanceof Blob); // true
+          //  console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
 
-            let newShout = new FormData();
-            newShout.append('shoutImage', this.state.selectedFile);
-            newShout.append('shoutIp', this.state.shoutIp);
-            newShout.append('shoutEntry', this.state.shoutEntry);
-            newShout.append('shoutLat', this.props.myUserLocation.lat);
-            newShout.append('shoutLong', this.props.myUserLocation.lng)
-            this.props.addShout(newShout);
-            this.refs.addDialog.hide();
+            var options = {
+                maxSizeMB: 1,
+                maxWidthOrHeight: 1920,
+                useWebWorker: true
+            }
+            this.setState({progressBarStatus: 10})
+            imageCompression(imageFile, options)
+                .then((compressedFile) => {
+                //    console.log('compressedFile instanceof Blob', compressedFile instanceof Blob); // true
+                //    console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
 
-        })
-        .catch(err => console.error(err));
+                    this.setState({selectedFile: compressedFile});
 
-}else{
-    toast.error("Only JPG/PNG/GIF of 2MB or less allowed.", {
+                    this.setState({progressBarStatus: 50})
+                    //Get IP
+                    fetch(IP_URL)
+                        .then((response) => response.json())
+                        .then((responseData) => {
+
+                            this.setState({
+                                shoutIp: responseData['ip'],
+                            });
+                            this.setState({progressBarStatus: 80})
+                            let newShout = new FormData();
+                            newShout.append('shoutImage', this.state.selectedFile);
+                            newShout.append('shoutIp', this.state.shoutIp);
+                            newShout.append('shoutEntry', this.state.shoutEntry);
+                            newShout.append('shoutLat', this.props.myUserLocation.lat);
+                            newShout.append('shoutLong', this.props.myUserLocation.lng)
+                            this.props.addShout(newShout);
+                            this.setState({progressBarStatus: 100})
+                            this.refs.addDialog.hide();
+
+                        })
+                        .catch(err => console.error(err));
+                })
+                .catch(err => console.error(err));
+        }else if (this.state.selectedFile.type === "image/gif" && this.state.selectedFile.size <= 1024000 ){
+            this.setState({progressBarStatus: 10})
+            //Get IP
+            fetch(IP_URL)
+                .then((response) => response.json())
+                .then((responseData) => {
+
+                    this.setState({
+                        shoutIp: responseData['ip'],
+                    });
+                    this.setState({progressBarStatus: 50})
+                    let newShout = new FormData();
+                    newShout.append('shoutImage', this.state.selectedFile);
+                    newShout.append('shoutIp', this.state.shoutIp);
+                    newShout.append('shoutEntry', this.state.shoutEntry);
+                    newShout.append('shoutLat', this.props.myUserLocation.lat);
+                    newShout.append('shoutLong', this.props.myUserLocation.lng)
+                    this.setState({progressBarStatus: 80})
+                    this.props.addShout(newShout);
+                    this.setState({progressBarStatus: 100})
+                    this.refs.addDialog.hide();
+
+                })
+                .catch(err => console.error(err));
+        }else{
+    toast.error("Only JPG/PNG/GIF of 1MB or less allowed.", {
         position: toast.POSITION.BOTTOM_LEFT
     });
-}
+        }
 
     }
 
@@ -66,6 +114,7 @@ if(this.state.selectedFile.size <= 2000000 &&
     }
 
     fileChangedHandler = (event) => {
+        this.setState({progressBarStatus: 0})
         this.setState({selectedFile: event.target.files[0]})
 
 
@@ -87,7 +136,9 @@ if(this.state.selectedFile.size <= 2000000 &&
                         <br/>
                         <Button variant="outlined" style={{marginRight: 10}} color="primary" onClick={this.handleSubmit}>Save</Button>
                         <Button variant="outlined" color="secondary" onClick={this.cancelSubmit}>Cancel</Button>
-
+                        <br/>
+                        <br/>
+                        <Progress type="circle" percent={this.state.progressBarStatus} status="active" />
                     </form>
                 </SkyLight>
                 <div>
