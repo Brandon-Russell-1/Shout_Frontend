2360ebfc3abf49292e1fb5cdae13cdf76246ec28
BwizLabs <brandon.r.russell.1@gmail.com>
2019-06-13 16:41:02 -0400

    tsing

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
 //export const SERVER_URL = 'http://localhost:8080'
 
+
+//Server
 export const SERVER_URL = 'https://dayshoutbackend.herokuapp.com'
 
 export const IP_URL = 'https://api.ipdata.co/?api-key=2b582d1c4181bc0085b1d0d1809de04e0f8ef8046b43dc4c0745060e'
